f764525e860a66930b7b24854aee554d385dbdd1
chbailey081 <39930155+chbailey081@users.noreply.github.com>
2020-07-04 14:55:29 -0400

    update
    updated landing page and Jumbo tron. Added additional styling to feature image and other CSS styling

--- HomePage.js
+++ HomePage.js
@@ -1,13 +1,13 @@
-var userName = prompt("what is your first name?");
-var userlastName = prompt("what is your last name?");
-alert("Nice to meet you, " + userName + " " +userlastName);
-console.log("Your full name is, " +userName +userlastName);
+//var userlastName = prompt("what is your last name?");
+//var userName = prompt("what is your first name?");
+//alert("Nice to meet you, " + userName + " " +userlastName);
+//console.log("Your full name is, " +userName +userlastName);
 
 
 
-var userAge = prompt("what is your age?");
-alert("You are " + userAge );
-console.log("You are " +userAge );
+//var userAge = prompt("what is your age?");
+//alert("You are " + userAge );
+//console.log("You are " +userAge );
 
 
 
